377e81ae946f6796b530e421c71d182dd70dca14
Lohith Sindiri <lohithsindiri@MacBook-Air.local>
2024-01-24 07:12:59 +0530

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return <div className="text-3xl font-bold">Welcome to OTT</div>;
+  return <Body />;
 }
 
 export default App;
